7412221dde105db8c10ab7037b3f8a4e175f1f27
Andy <andyleo97@gmail.com>
2018-11-13 20:43:32 -0600

    finish ls

--- ls.c
+++ ls.c
@@ -1,54 +1,80 @@
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <dirent.h>
-#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+int PATH_MAX = 200;
+
+void toLsString(struct stat fileStat[100], struct dirent files[100], int size);
 
 int main(int argc, char const *argv[]) {
   char cwd[PATH_MAX];
+  int size;
   if (getcwd(cwd, sizeof(cwd)) != NULL) {
     DIR *dir = opendir(cwd);
 
-
-    struct dirent *files[100];
+    struct dirent files[100];
     struct stat fileInfo[100];
 
     int i = 0;
+    int numOfFiles = 0;
     while (1) {
       struct dirent *temp;
       struct stat temp2;
 
       if ((temp = readdir(dir)) != NULL) {
-        char* str = temp->d_name;
-        if(str[0] != '.'){
-        printf("%s\n", temp->d_name);
-        files[i] = temp;
-      }
-      }
-      else{
+        struct dirent temp1 = *temp;
+        char *str = temp1.d_name;
+        if (str[0] != '.') {
+          files[numOfFiles] = temp1;
+          if ((lstat(temp->d_name, &temp2)) != -1) {
+            fileInfo[numOfFiles] = temp2;
+          }
+          numOfFiles++;
+        }
+      } else {
         break;
       }
 
-
-      int statCheck = lstat(temp->d_name , &temp2);
-      if((statCheck) != -1){
-        fileInfo[i] = temp2;
-      }
-
       i++;
-
     }
- // i = 0;
- //    while (i < 6) {
- //      printf("%s\n", files[i]->d_name);
- //      i++;
- //    }
-  }
-  else {
-  printf("failed\n");
+    toLsString(fileInfo, files, numOfFiles);
+
+  } else {
+    printf("failed\n");
   }
 
   return 0;
+}
+
+void toLsString(struct stat fileStat[100], struct dirent files[100], int size) {
+  int i = 0;
+  while (i < size) {
+    printf((S_ISDIR(fileStat[i].st_mode)) ? "d" : "-");
+    printf((fileStat[i].st_mode & S_IRUSR) ? "r" : "-");
+    printf((fileStat[i].st_mode & S_IWUSR) ? "w" : "-");
+    printf((fileStat[i].st_mode & S_IXUSR) ? "x" : "-");
+    printf((fileStat[i].st_mode & S_IRGRP) ? "r" : "-");
+    printf((fileStat[i].st_mode & S_IWGRP) ? "w" : "-");
+    printf((fileStat[i].st_mode & S_IXGRP) ? "x" : "-");
+    printf((fileStat[i].st_mode & S_IROTH) ? "r" : "-");
+    printf((fileStat[i].st_mode & S_IWOTH) ? "w" : "-");
+    printf((fileStat[i].st_mode & S_IXOTH) ? "x" : "-");
+    printf("  %u", fileStat[i].st_uid);
+    printf("  %u", fileStat[i].st_gid);
+
+    struct timespec time1 = fileStat[i].st_mtimespec;
+    time_t sec = (time_t)time1.tv_sec;
+    struct tm* date = localtime(&sec);
+
+    printf("  %d/%d/%d  %d:%d", date->tm_mon, date->tm_mday, (date->tm_year - 100), date->tm_hour, date->tm_min);
+    printf("  %lld", fileStat[i].st_size);
+    printf("  \t%lld", fileStat[i].st_blocks);
+    printf("  %s\n", files[i].d_name);
+    i++;
+  }
 }
